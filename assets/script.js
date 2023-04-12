d8c54e6b71f00354a359f64e7acb0e9ce60b54c0
Kalil J. Fakhouri <53912803+kaka-jaques@users.noreply.github.com>
2023-04-12 00:02:19 -0300

    Update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var taxaSelic;
+var taxaCdi;
+
 $.ajax({
     type: "GET",
     url: "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json",
@@ -6,9 +9,9 @@ $.ajax({
     },
     dataType: "json",
     success: function (response){
-        console.log(response.at(-1));
-        document.getElementById('selictx').valueAsNumber = response.at(-1).valor
-        document.getElementById('selictxtext').append(response.at(-1).data)
+        taxaSelic = response.at(-1).valor;
+        document.getElementById('selictx').valueAsNumber = taxaSelic * 100;
+        document.getElementById('selictxtext').append(response.at(-1).data);
     },
     error: function(error){
         console.log(error);
@@ -23,8 +26,8 @@ $.ajax({
     },
     dataType: "json",
     success: function (response){
-        console.log(response.at(-1));
-        document.getElementById('cditx').valueAsNumber = response.at(-1).valor
+        taxaCdi = response.at(-1).valor;
+        document.getElementById('cditx').valueAsNumber = taxaCdi;
         document.getElementById('cditxtext').append(response.at(-1).data)
     },
     error: function(error){
@@ -38,11 +41,9 @@ function monthlyApp(){
 
     if(checkbox.checked){
         document.getElementById('capMonth').removeAttribute('disabled');
-        console.log('checked');
     }else{
         document.getElementById('capMonth').valueAsNumber = null;
         document.getElementById('capMonth').setAttribute('disabled','');
-        console.log('discheked');
     }
 
 }
